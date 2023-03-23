bd7fda159f6820eb0209e8b8805d58ff059d6f7d
Valentin Bauer <valentin.bauer@tuwien.ac.at>
2023-03-23 01:59:51 +0100

    Change form data to fetch api, add config file

--- wakeup-service.py
+++ wakeup-service.py
@@ -2,13 +2,24 @@ import time
 import os
 import re
 import ipaddress
+import json
+import logging
+import configparser
 
 from gevent import monkey
 monkey.patch_all()
 
 from gevent.pywsgi import WSGIServer
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from wakeonlan import send_magic_packet
+from ping3 import ping
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Read configuration
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 class Machine:
     def __init__(self, ip_address, mac_address):
@@ -17,24 +28,18 @@ class Machine:
         self.last_successful_ping_time = None
 
     def ping(self) -> bool:
-        response = os.system("ping -c 1 " + self.ip_address)
-        
-        if response == 0:
+        response_time = ping(self.ip_address, timeout=1)
+        if response_time is not None:
             self.last_successful_ping_time = time.time()
             return True
-        
         return False
-            
+
     def wake(self):
         send_magic_packet(self.mac_address)
 
-machine = Machine('192.168.0.87', 'bc:ee:7b:8c:70:80')
+# Initialize the machine object using the configuration file
+machine = Machine(config.get('Machine', 'ip_address'), config.get('Machine', 'mac_address'))
 app = Flask(__name__)
-
-def unix_time_to_str(unix_time):
-    if not unix_time:
-        return 'Never'
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(unix_time))
 
 @app.route("/")
 def index():
@@ -44,61 +49,52 @@ def index():
                            last_successful_ping_time=machine.last_successful_ping_time)
 
 @app.route("/ping", methods=['POST'])
-def ping():
-    # get the ip address from the form data and update the machine status
-    
-    ip_address_invalid = False
-    ping_successful = None
-    
+def ping_machine():
     try:
-        new_ip_address = str(ipaddress.ip_address(request.form['ip_address']))
-        
+        data = request.get_json()
+        new_ip_address = str(ipaddress.ip_address(data['ip_address']))
+
         if new_ip_address != machine.ip_address:
             machine.ip_address = new_ip_address
-            print(f'IP address updated to: {machine.ip_address}')
-            
-        # ping the machine and update the last successful ping time
+            logging.info(f'IP address updated to: {machine.ip_address}')
+
         ping_successful = machine.ping()
-        print(f'Pinged, last successful ping time: {machine.last_successful_ping_time}')
-        
-    except ValueError:
-        ip_address_invalid = True
-        print(f'Invalid IP address entered: {request.form["ip_address"]}')        
-    
-    return render_template('index.html',
-                           ip_address=machine.ip_address,
-                           mac_address=machine.mac_address,
-                           last_successful_ping_time=machine.last_successful_ping_time,
-                           ping_successful=ping_successful,
-                           ip_address_invalid=ip_address_invalid)
+        logging.info(f'Pinged, last successful ping time: {machine.last_successful_ping_time}')
+
+        return jsonify(ip_address=machine.ip_address,
+                       last_successful_ping_time=machine.last_successful_ping_time,
+                       ping_successful=ping_successful)
+
+    except ValueError as e:
+        logging.error(f'Invalid IP address entered: {data["ip_address"]}')
+        return jsonify(error=str(e)), 400
 
 @app.route("/wol", methods=['POST'])
 def wake_on_lan():
-    # get the mac address from the form data and update the machine status
-    new_mac_address = request.form['mac_address']
-    mac_address_invalid = False
-    
-    if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", new_mac_address.lower()):
-        if new_mac_address != machine.mac_address:
-            machine.mac_address = new_mac_address
-            print(f'MAC address updated to: {machine.mac_address}')
-    else:
-        mac_address_invalid = True
-        print(f'Invalid MAC address entered: {new_mac_address}')
-    
-    # send the wake-on-lan packet
-    machine.wake()
-    print(f'Wake-on-LAN packet sent to {machine.mac_address}')
-    
-    return render_template('index.html',
-                           ip_address=machine.ip_address,
-                           mac_address=machine.mac_address,
-                           last_successful_ping_time=machine.last_successful_ping_time,
-                           mac_address_invalid=mac_address_invalid)
+    try:
+        data = request.get_json()
+        new_mac_address = data['mac_address']
+
+        if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", new_mac_address.lower()):
+            if new_mac_address != machine.mac_address:
+                machine.mac_address = new_mac_address
+                logging.info(f'MAC address updated to: {machine.mac_address}')
+        else:
+            raise ValueError("Invalid MAC address format.")
+
+        machine.wake()
+        logging.info(f'Wake-on-LAN packet sent to {machine.mac_address}')
+
+        return jsonify(mac_address=machine.mac_address,
+                       last_successful_ping_time=machine.last_successful_ping_time)
+
+    except ValueError as e:
+        logging.error(f'Invalid MAC address entered: {new_mac_address}')
+        return jsonify(error=str(e)), 400
 
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    port = 8000
-    print(f'Starting server on {host}:{port}...')
+    host = config.get('Server', 'host')
+    port = config.getint('Server', 'port')
+    logging.info(f'Starting server on http://{host}:{port}...')
     http_server = WSGIServer((host, port), app)
     http_server.serve_forever()
